8ab7e210871fec118df0ea16d4a706ff92e00326
zesha5816 <zesha5816@wrdsb.ca>
2020-10-02 14:47:58 +0000

    comments

--- Main.java
+++ Main.java
@@ -12,10 +12,14 @@ public class Main {
   public static void main(String[] args) {
     // Create the Scanner
 		Scanner input = new Scanner(System.in);
+		//prompts user for input
 		System.out.println("Please enter an integer to count down to");
+		//get input from user
 		int countNumber = input.nextInt();
+		//set the value fo int as 100
 		int count = 100;
 		System.out.println("Count Down:");
+		//subtracts 5 from 100 until it getrs the closes to the original number
 		do {
 			System.out.println(count);
 			count = count - 5; 
